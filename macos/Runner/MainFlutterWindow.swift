1611bfafb4bae287b47bb183ea55ee4d3ed1d4e2
Mark Andrey <markandreydelacruz@ymail.com>
2026-04-06 11:46:10 +0800

    Adjust window size and content layout in MainFlutterWindow

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,19 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    let phoneSizedContent = NSSize(width: 393, height: 852)
+    let frame = self.frame
+    let resizedFrame = NSRect(
+      x: frame.midX - (phoneSizedContent.width / 2),
+      y: frame.midY - (phoneSizedContent.height / 2),
+      width: phoneSizedContent.width,
+      height: phoneSizedContent.height
+    )
+
     self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+    self.setContentSize(phoneSizedContent)
+    self.setFrame(resizedFrame, display: true)
+    self.minSize = NSSize(width: 420, height: 640)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
